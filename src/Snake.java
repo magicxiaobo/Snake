fed3d7d82ed6adb30b8b64be3fb9dc41bb568b64
magicxiaobo <yuxiaobohit@gmail.com>
2014-09-18 22:48:07 -0700

    method draw

--- src/Snake.java
+++ src/Snake.java
@@ -1,3 +1,5 @@
+import java.awt.Color;
+import java.awt.Graphics;
 
 public class Snake {
 	Node head = null;
@@ -6,7 +8,21 @@ public class Snake {
 	
 	
 	private class Node {
+		int w = Yard.BLOCK_SIZE;
+		int h = Yard.BLOCK_SIZE;
+		int row, col;
 		
+		Node(int row, int col) {
+			this.row = row;
+			this.col = col;
+		}
+		
+		void draw(Graphics g) {
+			Color c = g.getColor();
+			g.setColor(Color.BLACK);
+			g.fillRect(Yard.BLOCK_SIZE * row, Yard.BLOCK_SIZE * col, w, h);
+			g.setColor(c);
+		}
 	}
 	
 }
